b3c0211b190f659a4471519dee25a87f7dccc783
scott <scottswindell@email.arizona.edu>
2017-08-30 11:12:29 -0700

    New method in Boltwood class to circumvent the bad firmware thresholds.

--- src/slotis_device/boltwood.py
+++ src/slotis_device/boltwood.py
@@ -23,8 +23,34 @@ class Boltwood:
 		
 		return jdata
 
+
+	def getdata2(self):
+		"""Threshold for dayCond is too high so we figure it out here"""
+		try:
+			s = scottSock(self.ip, self.port)
+			data = s.converse("\n")
+			jdata = json.loads(data)
+			jdata["roofCloseRequested"] = 0
+
+			if int( jdata["daylightADC"]) > 600:
+				jdata["roofCloseRequested"] = 1
+			
+			elif int( jdata["cloudCond"] ) > 2:
+				jdata["roofCloseRequested"] = 1
+			
+			s.close()
+		except Exception as err:
+			print err
+			jdata = {}
+			jdata["error"] = err
+			jdata["roofCloseRequested"] = 1
+			
+		
+		return jdata
+
 	def isSafe(self):
-		jdata = self.getdata()
+		jdata = self.getdata2()
+		
 		if jdata["roofCloseRequested"] == 1:
 			return 0
 		else:
